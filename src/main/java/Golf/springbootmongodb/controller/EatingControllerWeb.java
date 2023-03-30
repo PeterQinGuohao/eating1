584fd03bb53e4d4e3b2569432c62838440bc9452
PeterQinGuohao <qin.g@northeastern.edu>
2023-03-30 15:04:47 -0700

    add website for update and delete

--- src/main/java/Golf/springbootmongodb/controller/EatingControllerWeb.java
+++ src/main/java/Golf/springbootmongodb/controller/EatingControllerWeb.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class EatingControllerWeb {
@@ -23,9 +24,24 @@ public class EatingControllerWeb {
         return "Hello World !!!";
     }
 
-    @GetMapping("/index")
-    public String index(Model model) {
-        return "index";
+    @GetMapping("/eatingAdd")
+    public String add(Model model) {
+        return "eatingAdd";
+    }
+
+    @GetMapping("/oneEating")
+    public String getOne(Model model) {
+        return "oneEating";
+    }
+
+    @GetMapping("/eatingUpdate")
+    public String update(Model model) {
+        return "eatingUpdate";
+    }
+
+    @GetMapping("/eatingDelete")
+    public String delete(Model model) {
+        return "eatingDelete";
     }
 
     @GetMapping("/web/eating")
@@ -36,8 +52,56 @@ public class EatingControllerWeb {
     }
 
     @PostMapping("/web/eating/save")
-    public String saveEating(Model model,EatingDTO eating) {
-        eatingRepo.save(eating);
-        return "redirect:/web/eating";
+    public String saveEating(Model model, EatingDTO eating) {
+        //eatingRepo.save(eating);
+        //return "redirect:/web/eating";
+        Optional<?> eatingOptional = Optional.ofNullable(eatingRepo.findByDate(eating.getDate()));
+        if (eatingOptional.isPresent()) {
+            return "dateExist";
+        } else {
+            eatingRepo.save(eating);
+            return "redirect:/web/eating";
+        }
+    }
+
+    @GetMapping("/web/eating/getDay")
+    public String getByDate(Model model, String date) {
+        Optional<?> eatingOptional = Optional.ofNullable(eatingRepo.findByDate(date));
+        if (eatingOptional.isPresent()) {
+            EatingDTO eating = eatingRepo.findByDate(date);
+            model.addAttribute("date", eating.getDate());
+            model.addAttribute("calories", eating.getCalories());
+            model.addAttribute("food", eating.getFood());
+            return "getByDate";
+        } else {
+            return "blank";
+        }
+    }
+
+    @PostMapping("/web/eating/update")
+    public String updateEating(Model model, String datecheck,EatingDTO eating) {
+        Optional<?> eatingOptional = Optional.ofNullable(eatingRepo.findByDate(datecheck));
+        if (eatingOptional.isPresent()) {
+            EatingDTO preEating = eatingRepo.findByDate(datecheck);
+            preEating.setCalories(eating.getCalories());
+            preEating.setFood(eating.getFood() != null ? eating.getFood() : preEating.getFood());
+            eatingRepo.save(preEating);
+            return "redirect:/web/eating";
+        } else {
+            return "blank";
+        }
+
+    }
+
+    @PostMapping("/web/eating/delete")
+    public String deleteEating(Model model, String date) {
+        Optional<?> eatingOptional = Optional.ofNullable(eatingRepo.findByDate(date));
+        if (eatingOptional.isPresent()) {
+            EatingDTO eating = eatingRepo.findByDate(date);
+            eatingRepo.delete(eating);
+            return "redirect:/web/eating";
+        } else {
+            return "blank";
+        }
     }
 }
